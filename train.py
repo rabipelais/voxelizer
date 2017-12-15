0eb01d0e2b700f463d8e93caf47ada667f21c6e1
Sebastian Mendez <sebas.chinoir@gmail.com>
2017-12-15 13:05:18 +0100

    Rewrite graph and add summaries.

--- train.py
+++ train.py
@@ -9,13 +9,17 @@ def conv3d(x, W):
     return tf.nn.conv3d(x, W, strides=[1, 1, 1, 1, 1], padding='SAME')
 
 
-train_data_dir = "preprocessed"
-
-labels_dict = {}
-current_label_idx = 0
-vx_width = 64
-vx_height = 64
-vx_depth = 64
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(var))
+            tf.summary.scalar('min', tf.reduce_min(var))
+            tf.summary.histogram('histogram', var)
 
 
 def weight_variable(shape):
@@ -30,129 +34,122 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def build_graph(x):
+def conv_layer(input_tensor, input_dim, output_dim, layer_name):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            W_conv = weight_variable([3, 3, 3, input_dim, output_dim])
+            variable_summaries(W_conv)
+        with tf.name_scope('biases'):
+            b_conv = bias_variable([output_dim])
+            variable_summaries(b_conv)
+        with tf.name_scope('convolution'):
+            preactivate = conv3d(input_tensor, W_conv) + b_conv
+            tf.summary.histogram('pre_activations', preactivate)
+        activations = tf.nn.relu(preactivate)
+        tf.summary.histogram('activations', activations)
+        return activations
+
+
+def pooling_layer(input_tensor, layer_name):
+    with tf.name_scope(layer_name):
+        pooled = tf.nn.max_pool3d(input_tensor, ksize=[1, 2, 2, 2, 1],
+                                  strides=[1, 2, 2, 2, 1], padding='SAME')
+        tf.summary.histogram('pooled', pooled)
+    return pooled
+
+
+def build_graph(x, y_, res):
     with tf.name_scope('reshape'):
-        x_voxel = tf.reshape(x, [-1, vx_width, vx_height, vx_depth, 1])
+        x_voxel = tf.reshape(x, [-1, res, res, res, 1])
 
-    with tf.name_scope('conv1'):
-        W_conv1 = weight_variable([3, 3, 3, 1, 8])
-        b_conv1 = bias_variable([8])
-        h_conv1 = tf.nn.relu(conv3d(x_image, W_conv1) + b_conv1)
+    h_conv1 = conv_layer(x_voxel, 1, 8, "conv1")
+    h_pool1 = pooling_layer(h_conv1, 'pool1')
 
-    with tf.name_scope('pool1'):
-        h_pool1 = tf.nn.max_pool3d(h_conv1, ksize=[1, 2, 2, 2, 1],
-                                   strides=[1, 2, 2, 2, 1], padding='SAME')
+    h_conv2 = conv_layer(h_pool1, 8, 14, "conv2")
+    h_pool2 = pooling_layer(h_conv2, "pool2")
 
-    with tf.name_scope('conv2'):
-        W_conv2 = weight_variable([3, 3, 3, 8, 14])
-        b_conv2 = bias_variable([14])
-        h_conv2 = tf.nn.relu(conv3d(h_pool1, W_conv2) + b_conv2)
+    h_conv3 = conv_layer(h_pool2, 14, 14, "conv3")
+    h_pool3 = pooling_layer(h_conv3, "pool3")
 
-    with tf.name_scope('pool2'):
-        h_pool2 = tf.nn.max_pool3d(h_conv2, ksize=[1, 2, 2, 2, 1],
-                                   strides=[1, 2, 2, 2, 1], padding='SAME')
+    h_conv4 = conv_layer(h_pool3, 14, 20, "conv4")
+    h_pool4 = pooling_layer(h_conv4, "pool4")
 
-    with tf.name_scope('conv3'):
-        W_conv3 = weight_variable([3, 3, 3, 14, 14])
-        b_conv3 = bias_variable([14])
-        h_conv3 = tf.nn.relu(conv3d(h_pool2, W_conv2) + b_conv2)
+    h_conv5 = conv_layer(h_pool4, 20, 20, "conv5")
+    h_pool5 = pooling_layer(h_conv5, "pool5")
 
-    with tf.name_scope('pool3'):
-        h_pool3 = tf.nn.max_pool3d(h_conv3, ksize=[1, 2, 2, 2, 1],
-                                   strides=[1, 2, 2, 2, 1], padding='SAME')
+    h_conv6 = conv_layer(h_pool5, 20, 26, "conv6")
+    h_pool6 = pooling_layer(h_conv6, "pool6")
 
-    with tf.name_scope('conv4'):
-        W_conv4 = weight_variable([3, 3, 3, 14, 20])
-        b_conv4 = bias_variable([20])
-        h_conv4 = tf.nn.relu(conv3d(h_pool3, W_conv2) + b_conv3)
+    h_conv7 = conv_layer(h_pool6, 26, 26, "conv7")
+    h_conv8 = conv_layer(h_conv7, 26, 32, "conv8")
+    h_conv9 = conv_layer(h_conv8, 32, 32, "conv9")
+    h_conv10 = conv_layer(h_conv9, 32, 32, "conv10")
 
-    with tf.name_scope('pool4'):
-        h_pool4 = tf.nn.max_pool3d(h_conv4, ksize=[1, 2, 2, 2, 1],
-                                   strides=[1, 2, 2, 2, 1], padding='SAME')
-
-    with tf.name_scope('conv5'):
-        W_conv5 = weight_variable([3, 3, 3, 20, 20])
-        b_conv5 = bias_variable([20])
-        h_conv5 = tf.nn.relu(conv3d(h_pool4, W_conv2) + b_conv4)
-
-    with tf.name_scope('pool5'):
-        h_pool5 = tf.nn.max_pool3d(h_conv5, ksize=[1, 2, 2, 2, 1],
-                                   strides=[1, 2, 2, 2, 1], padding='SAME')
-
-    with tf.name_scope('conv6'):
-        W_conv6 = weight_variable([3, 3, 3, 20, 26])
-        b_conv6 = bias_variable([26])
-        h_conv6 = tf.nn.relu(conv3d(h_pool5, W_conv2) + b_conv5)
-
-    with tf.name_scope('pool6'):
-        h_pool6 = tf.nn.max_pool3d(h_conv6, ksize=[1, 2, 2, 2, 1],
-                                   strides=[1, 2, 2, 2, 1], padding='SAME')
-
-    with tf.name_scope('conv7'):
-        W_conv7 = weight_variable([3, 3, 3, 26, 26])
-        b_conv7 = bias_variable([26])
-        h_conv7 = tf.nn.relu(conv3d(h_conv6, W_conv6) + b_conv6)
-    with tf.name_scope('conv8'):
-        W_conv8 = weight_variable([3, 3, 3, 26, 32])
-        b_conv8 = bias_variable([32])
-        h_conv8 = tf.nn.relu(conv3d(h_conv7, W_conv7) + b_conv7)
-    with tf.name_scope('conv9'):
-        W_conv9 = weight_variable([3, 3, 3, 32, 32])
-        b_conv9 = bias_variable([32])
-        h_conv9 = tf.nn.relu(conv3d(h_conv8, W_conv8) + b_conv8)
-    with tf.name_scope('conv10'):
-        W_conv10 = weight_variable([3, 3, 3, 32, 32])
-        b_conv10 = bias_variable([32])
-        h_conv10 = tf.nn.relu(conv3d(h_conv9, W_conv9) + b_conv9)
-
-    with tf.name_scope('pool10'):
-        h_pool10 = tf.nn.max_pool3d(h_conv10, ksize=[1, 2, 2, 2, 1],
-                                    strides=[1, 2, 2, 2, 1], padding='SAME')
+    h_pool10 = pooling_layer(h_conv10, 'pool10')
 
     # Reshape and fully connected
     with tf.name_scope('dropout'):
-        h_pool10_flat = tf.reshape(-1, 32 * 8 * 8 * 8)
+        h_pool10_flat = tf.reshape(h_pool10, [-1, 32 * 8 * 8 * 8])
+        # To be able to turn it off during testing
         keep_prob = tf.placeholder(tf.float32)
         h_pool_drop = tf.nn.dropout(h_pool10_flat, keep_prob)
 
-        W_fc1 = weight_variable([32 * 8 * 8 * 8, 512])
-        b_fc1 = bias_variable([512])
-        h_fc1 = tf.nn.relu(tf.matmul(h_pool_drop, W_fc1) + b_fc1)
+    with tf.name_scope('fully_connected'):
+        with tf.name_scope('weights'):
+            W_fc1 = weight_variable([32 * 8 * 8 * 8, 512])
+            variable_summaries(W_fc1)
+        with tf.name_scope('biases'):
+            b_fc1 = bias_variable([512])
+            variable_summaries(b_fc1)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(h_pool_drop, W_fc1) + b_fc1
+            tf.summary.histogram('pre_activations', preactivate)
+        h_fc1 = tf.nn.relu(preactivate)
+        tf.summary.histogram('activations', h_fc1)
 
-        W_fc2 = weight_variable([512, 10])
-        b_fc2 = bias_variable([10])
+    with tf.name_scope('readout'):
+        with tf.name_scope('weights'):
+            W_fc2 = weight_variable([512, 10])
+            variable_summaries(W_fc2)
+        with tf.name_scope('biases'):
+            b_fc2 = bias_variable([10])
+            variable_summaries(b_fc2)
+        with tf.name_scope('Wx_plus_b'):
+            y_readout = tf.matmul(h_fc1, W_fc2) + b_fc2
+            tf.summary.histogram('pre_activations', y_readout)
 
-        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    with tf.name_scope('cross_entropy'):
+        # The raw formulation of cross-entropy,
+        #
+        # tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.softmax(y)),
+        #                               reduction_indices=[1]))
+        #
+        # can be numerically unstable.
+        #
+        # So here we use tf.nn.softmax_cross_entropy_with_logits on the
+        # raw outputs of the nn_layer above, and then average across
+        # the batch.
+        diff = tf.nn.softmax_cross_entropy_with_logits(
+            labels=y_, logits=y_readout)
+        with tf.name_scope("total"):
+            cross_entropy = tf.reduce_mean(diff)
+    tf.summary.scalar("cross_entropy", cross_entropy)
 
-    return y_conv, keep_prob
+    learning_rate = 0.1
+    with tf.name_scope("train"):
+        train_step = tf.train.AdamOptimizer(0.1).minimize(cross_entropy)
 
+    with tf.name_scope('accuracy'):
+        with tf.name_scope("correct_prediction"):
+            correct_prediction = tf.equal(
+                tf.arg_max(y_readout, 1), tf.arg_max(y_, 1))
+        with tf.name_scope("accuracy"):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.summary.scalar("accuracy", accuracy)
 
-def read_data():
-    # Read the data
-    for (dirpath, dirnames, filenames) in os.walk(train_data_dir):
-        data_list = []
-        labels_list = []
-        for name in files:
-            basename, ext = os.path.spliteext(name)
-            if ext != 'vox':
-                break
-            grid = read_grid(os.path.join(dirpath, name))
-            data.append(grid)
-            train, label_name, number = tuple(basename.split('_'))
+    merged = tf.summary.merge_all()
 
-            label_idx = -1
-            if label_name in labels_dict:
-                label_idx = labels_dict[label_name]
-            else:
-                labels_dict[label_name] = current_label_idx
-                label_idx = current_label_idx
-                curren_label_idx += 1
-
-                labels_list.append(labels_idx)
-
-        labels_one_hot = tf.one_hot(labels_list, len(labels_dict))
-
-        return data_list, labels_one_hot
+    return y_readout, merged
 
 
 def main(_):
@@ -213,7 +210,6 @@ def parse_function(width, height, depth, record):
         'height': tf.FixedLenFeature((), tf.int64),
         'depth': tf.FixedLenFeature((), tf.int64),
         'data': tf.FixedLenFeature((width, height, depth), tf.float32),
-        # TODO: Make this dynamic
         'label_one_hot': tf.VarLenFeature(tf.int64)
     }
 
@@ -222,7 +218,6 @@ def parse_function(width, height, depth, record):
     height = parsed_features['height']
     depth = parsed_features['depth']
     dense_labels = tf.sparse_tensor_to_dense(parsed_features['label_one_hot'])
-    print dense_labels
     return parsed_features['data'], dense_labels
 
 
@@ -239,7 +234,6 @@ if __name__ == '__main__':
     dataset = dataset.shuffle(shuffle_size)
     dataset = dataset.repeat()
     dataset = dataset.batch(batch_size)
-    print dataset.output_shapes
 
     features, labels = dataset.make_one_shot_iterator().get_next()
 
